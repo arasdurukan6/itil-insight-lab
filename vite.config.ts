da406fa52fb36b353d8b1b1fcd4f3959af7d4719
arasdurukan6 <arasdurukan6@gmail.com>
2026-05-28 14:16:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   base: '/itil-insight-lab/',
   tanstackStart: {
+    spa: true,
     server: { entry: "server" },
   },
 });
